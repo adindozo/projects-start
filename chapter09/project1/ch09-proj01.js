47bd9451ffe288aaf79a03618721fbce522cca23
adindozo <adin.dozo1@outlook.com>
2022-11-19 17:16:13 +0100

    media player, DOM manipulations and event handling

--- chapter09/project1/ch09-proj01.js
+++ chapter09/project1/ch09-proj01.js
@@ -1,4 +1,26 @@
+window.onload = function(){
+
+
+
 const symbolPlay = '⯈';
 const symbolPause = '❚ ❚';
 const files = ['Nature-8399','River-655','Waterfall-941','Wave-2737'];
 
+
+console.log(2)
+for(let file of files){
+    let img = document.createElement('img');
+    img.src='images/'+file+ '.jpg';
+    img.addEventListener('click',(e)=>{
+        document.querySelector('#vidPlayer source').setAttribute('src', `videos/${file}.mp4`);
+        console.log(document.querySelector('#vidPlayer source').getAttribute('src'));
+
+        
+        document.querySelector('#vidPlayer').load();
+        document.querySelector('#vidPlayer').pause();
+    })
+
+     document.querySelector('aside').appendChild(img);
+}
+
+}
