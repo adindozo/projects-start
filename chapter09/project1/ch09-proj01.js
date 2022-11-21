15abd90101a225da5184578f7d35bfdd4011b340
adindozo <adin.dozo1@outlook.com>
2022-11-21 10:58:05 +0100

    Painting Viewer, project 2, JS event handling and delegation

--- chapter09/project1/ch09-proj01.js
+++ chapter09/project1/ch09-proj01.js
@@ -7,20 +7,72 @@ const symbolPause = '❚ ❚';
 const files = ['Nature-8399','River-655','Waterfall-941','Wave-2737'];
 
 
-console.log(2)
+
+document.querySelector('button[data-skip="5"]').addEventListener('click',()=>{
+    document.querySelector('#vidPlayer').currentTime+=parseInt(document.querySelector('button[data-skip="5"]').dataset.skip);
+    console.log(document.querySelector('button[data-skip="5"]').dataset.skip)
+})
+
+document.querySelector('button[data-skip="-5"]').addEventListener('click',()=>{
+    document.querySelector('#vidPlayer').currentTime+=parseInt(document.querySelector('button[data-skip="-5"]').dataset.skip);
+})
+
+
 for(let file of files){
     let img = document.createElement('img');
     img.src='images/'+file+ '.jpg';
     img.addEventListener('click',(e)=>{
         document.querySelector('#vidPlayer source').setAttribute('src', `videos/${file}.mp4`);
-        console.log(document.querySelector('#vidPlayer source').getAttribute('src'));
+        
 
         
         document.querySelector('#vidPlayer').load();
-        document.querySelector('#vidPlayer').pause();
+        if(document.querySelector('#play').dataset.paused=='0'){
+           
+            document.querySelector('#play').dataset.paused='1';
+            document.querySelector('#play').innerHTML='⯈';
+            document.querySelector('#vidPlayer').pause();
+           
+            
+        } 
+   
     })
 
      document.querySelector('aside').appendChild(img);
 }
+
+document.querySelector('#stop').addEventListener('click',()=>{
+    document.querySelector('#vidPlayer').pause();
+    document.querySelector('#vidPlayer').currentTime=0;
+    if(document.querySelector('#play').dataset.paused=='0'){
+           
+        document.querySelector('#play').dataset.paused='1';
+        document.querySelector('#play').innerHTML='⯈';
+        document.querySelector('#vidPlayer').pause();
+       
+        
+    } 
+
+})
+//⏸
+document.querySelector('#play').addEventListener('click',()=>{
+    if(document.querySelector('#play').dataset.paused=='1'){
+        document.querySelector('#play').innerHTML='⏸';
+        document.querySelector('#play').dataset.paused='0';
+        document.querySelector('#vidPlayer').play();
+        
+    console.log(document.querySelector('#play').dataset.paused)
+
+        
+    } else {
+    document.querySelector('#play').dataset.paused='1';
+    document.querySelector('#play').innerHTML='⯈';
+    document.querySelector('#vidPlayer').pause();
+   
+    
+    }
+    
+})
+
 
 }
--- chapter09/project2/ch09-proj2.js
+++ chapter09/project2/ch09-proj2.js
@@ -1,1 +1,27 @@
 /* add your code here */
+window.onload=() => {
+    let paintings = JSON.parse(content);
+    console.log(paintings);
+    for(let painting of paintings){
+        let img = document.createElement('img');
+        img.src='images/small/'+painting.id+'.jpg';
+        img.dataset.id=painting.id;
+        let li = document.createElement('li');
+        li.appendChild(img);
+        document.querySelector('ul').appendChild(li);
+    }
+    document.querySelector('ul').addEventListener('click',(e) => {
+        if(e.target.nodeName=='IMG'){
+            document.querySelector('figure').innerHTML='';
+            let img = document.createElement('img');
+            img.src='images/large/'+e.target.dataset.id+'.jpg';
+            document.querySelector('figure').appendChild(img);
+            document.querySelector('h2').innerHTML=paintings.find(element => element.id==e.target.dataset.id).title;
+            document.querySelector('h3').innerHTML=paintings.find(element => element.id==e.target.dataset.id).artist;
+            let features = paintings.find(element => element.id==e.target.dataset.id).features;
+            for(let feature of features){
+                //todo generate rects, step 5
+            }
+        }
+    })
+}
