69f3696962fe855a0be00ce2219907b5a885fdce
adindozo <adin.dozo1@outlook.com>
2022-11-21 19:40:38 +0100

    Stock Portfolio Dashboard user list

--- chapter09/project3/ch09-proj03.js
+++ chapter09/project3/ch09-proj03.js
@@ -1,2 +1,19 @@
 /* add your code here */
 
+ window.onload= () => {
+    let users = JSON.parse(userContent);
+    let stocks = JSON.parse(stockContent);
+    document.querySelector('.Details').style.display='none';
+    for(let user of users){
+        let li =document.createElement('li'); //name, surname syntax
+        li.innerText=user.user.firstname+', '+user.user.lastname;
+        li.dataset.id=user.id;
+        document.querySelector('.UserList ul').appendChild(li);
+    }
+    document.querySelector('.UserList').addEventListener((e) => {
+        if(e.target.nodeName=='LI'){
+            
+            document.querySelector('.Details').style.removeProperty('display'); 
+        }
+    })
+ }
