fcf47a3f3cee74d35578adbea8cb6e1d6c3c79ea
adindozo <adin.dozo1@outlook.com>
2022-11-24 11:46:04 +0100

    implemented save and delete buttons, fixed memory leak on multiple instances of event listeners on buttons

--- chapter09/project3/ch09-proj03.js
+++ chapter09/project3/ch09-proj03.js
@@ -4,19 +4,73 @@
     let users = JSON.parse(userContent);
     let stocks = JSON.parse(stockContent);
     document.querySelector('.Details').style.display='none';
-  
+    let generateUserList=function(users){
     for(let user of users){
         let li =document.createElement('li'); //name, surname syntax
         li.innerText=user.user.firstname+', '+user.user.lastname;
         li.dataset.id=user.id;
         document.querySelector('.UserList ul').appendChild(li);
     }
-    document.querySelector('.UserList').addEventListener('click',(e) => {
-        if(e.target.nodeName=='LI'){
-           
+    }
+    generateUserList(users);     
+    let currentUserID=0;  
+    document.querySelector('.UserList').addEventListener('click',(E) => {
+        if(E.target.nodeName=='LI'){
+            
+            //let E=e;
             document.querySelector('.Details').style.removeProperty('display'); 
             document.querySelector('.StockDetails').style.display='none';
-            let user = users.find((user) => user.id==e.target.dataset.id);
+            currentUserID=E.target.dataset.id;
+            
+            let user = users.find((user) => user.id==currentUserID);
+            console.log(user);
+            // var old_element = document.getElementById("btn");
+            // var new_element = old_element.cloneNode(true);
+            // old_element.parentNode.replaceChild(new_element, old_element);
+            let oldDel= document.getElementById('btnDelete');
+            let newDel= oldDel.cloneNode(true);
+            oldDel.parentNode.replaceChild(newDel,oldDel);
+            //removing old event listeners on buttons
+            let oldSav= document.getElementById('btnSave');
+            let newSav= oldSav.cloneNode(true);
+            oldSav.parentNode.replaceChild(newSav,oldSav);
+            
+            document.getElementById('btnSave').addEventListener('click',(e) => {
+                e.preventDefault();
+                document.querySelector('.Details').style.display='none';
+                    
+                    console.log(user);
+                    // firstname":"Jacquette",
+                    // "lastname":"Marion",
+                    // "email":"jmarion0@nyu.edu",
+                    // "address":"207 Ryan Avenue",
+                    // "city":"Lubbock",
+                    // "state":"Texas",
+                    // "zip":"79491"
+                    user.user.firstname=document.getElementById('firstname').value;
+                    user.user.lastname=document.getElementById('lastname').value;
+                    user.user.address=document.getElementById('address').value;
+                    user.user.city=document.getElementById('city').value;
+                    user.user.email=document.getElementById('email').value;
+                    E.target.innerText=user.user.firstname+', '+user.user.lastname;
+            
+            })
+            document.getElementById('btnDelete').addEventListener('click',(e) => {
+                e.preventDefault();
+                document.querySelector('.Details').style.display='none';
+                
+                for(let i=0;i<users.length;i++){
+                    if(users[i].id==currentUserID){
+                        users.splice(i,1);
+                        break;
+                    }
+                }
+                
+                document.querySelector('.UserList ul').innerHTML='';
+                generateUserList(users);   
+             
+            
+            })
             document.getElementById('firstname').value=user.user.firstname;
             document.getElementById('lastname').value=user.user.lastname;
             document.getElementById('address').value=user.user.address;
@@ -34,6 +88,7 @@
                     document.getElementById('stockSector').innerText=stockinfo.sector;
                     document.getElementById('stockIndustry').innerText=stockinfo.subIndustry;
                     document.getElementById('stockAddress').innerText=stockinfo.address;
+                    document.getElementById('logo').src=`logos/${stock.symbol}.svg`
                     document.querySelector('.StockDetails').style.removeProperty('display'); 
                 })
                 symbol.innerText=stock.symbol;
