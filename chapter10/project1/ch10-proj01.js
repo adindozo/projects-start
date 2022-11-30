187a59f283444b28fb8055a670cf441d5f91a2cd
adindozo <adin.dozo1@outlook.com>
2022-11-30 16:05:47 +0100

    color schemes viewer, displayed list of colors from API

--- chapter10/project1/ch10-proj01.js
+++ chapter10/project1/ch10-proj01.js
@@ -1,9 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+document.addEventListener("DOMContentLoaded", function () {
+   document.getElementById('loader').style.display = 'inline-block';
    const url = "https://www.randyconnolly.com/funwebdev/3rd/api/colors/sample-colors.php";
-   
+   fetch(url).then(res => res.json()).then((data) => {
+      console.log(data);
+      
+      
+      for (let color of data) {
+         document.getElementById('loader').style.display = 'none';
+         console.log(data)
+         let article = document.createElement('article');
+         let divActions = document.createElement('div');
+         let h3 = document.createElement('h3');
+         let button = document.createElement('button');
+         let section = document.createElement('section');
+         let divPreview = document.createElement('div');
+
+         divPreview.className = 'preview';
+         divActions.className = 'actions';
+         article.className = "scheme-group";
+         section.className = 'scheme';
+
+         button.dataset.id=color.id;
+         button.innerText='View';
+         divActions.appendChild(button);
+         for (let scheme of color.scheme) {
+            let div = document.createElement('div');
+            div.className = 'color-box';
+            div.style.backgroundColor = scheme.web;
+            divPreview.appendChild(div);
+         }
+         section.appendChild(divPreview);
+         section.appendChild(divActions);
+         h3.innerText=color.title;
+         article.appendChild(h3);
+         article.appendChild(section);
+         document.querySelector('main').appendChild(article);
+      }
+   })
+
 });
 
 
 
- 
+
