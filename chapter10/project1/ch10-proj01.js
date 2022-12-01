afe455abc2426f7509c27e05ade1dd5934a73ff3
adindozo <adin.dozo1@outlook.com>
2022-12-01 10:17:18 +0100

    color schemes viewer, added view details section

--- chapter10/project1/ch10-proj01.js
+++ chapter10/project1/ch10-proj01.js
@@ -1,13 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
    document.getElementById('loader').style.display = 'inline-block';
    const url = "https://www.randyconnolly.com/funwebdev/3rd/api/colors/sample-colors.php";
+
    fetch(url).then(res => res.json()).then((data) => {
-      console.log(data);
-      
-      
+
+
+      document.querySelector('main').addEventListener('click', (e) => {
+         if (e.target.nodeName == 'BUTTON') {
+            let colorObject = data.find((color) => {
+               return color.id == e.target.dataset.id;
+            })
+            let h2 = document.createElement('h2');
+            h2.innerText = colorObject.title;
+            let aside=document.querySelector('aside');
+            aside.innerHTML='';
+            aside.appendChild(h2);
+            let fieldset = document.createElement('fieldset');
+            colorObject.scheme.forEach(scheme => {
+               let detailBox = document.createElement('div');
+               let colorRow = document.createElement('div');
+               
+               
+               let hex = document.createElement('span');
+               let rgb = document.createElement('span');
+               let label = document.createElement('label');
+
+              
+               detailBox.style.backgroundColor = scheme.web;
+               hex.innerText=scheme.web;
+               rgb.innerText=`rgb(${scheme.color.red},${scheme.color.green},${scheme.color.blue})`
+               detailBox.className = 'detailBox';
+               colorRow.className = 'colorRow';
+               label.innerText=scheme.name;
+               colorRow.appendChild(detailBox);
+               colorRow.appendChild(hex);
+               colorRow.appendChild(rgb);
+               colorRow.appendChild(label);
+               fieldset.appendChild(colorRow);
+               aside.appendChild(fieldset);
+            });
+
+         }
+
+      })
       for (let color of data) {
          document.getElementById('loader').style.display = 'none';
-         console.log(data)
+
          let article = document.createElement('article');
          let divActions = document.createElement('div');
          let h3 = document.createElement('h3');
@@ -20,8 +58,8 @@ document.addEventListener("DOMContentLoaded", function () {
          article.className = "scheme-group";
          section.className = 'scheme';
 
-         button.dataset.id=color.id;
-         button.innerText='View';
+         button.dataset.id = color.id;
+         button.innerText = 'View';
          divActions.appendChild(button);
          for (let scheme of color.scheme) {
             let div = document.createElement('div');
@@ -31,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
          }
          section.appendChild(divPreview);
          section.appendChild(divActions);
-         h3.innerText=color.title;
+         h3.innerText = color.title;
          article.appendChild(h3);
          article.appendChild(section);
          document.querySelector('main').appendChild(article);
